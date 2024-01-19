2109ec21793e766e1f5b16522c9982a7d39d19db
LaurieWired <123765654+LaurieWired@users.noreply.github.com>
2024-01-18 21:08:21 -0800

    Update keys and note output times

--- binary_player.py
+++ binary_player.py
@@ -27,6 +27,30 @@ b_minor_notes = [
     'B7', 'C#8/Db8', 'D8', 'E8', 'F#8/Gb8', 'G8', 'A8'
 ]
 
+d_minor_notes = [
+    'D0', 'E0', 'F0', 'G0', 'A0', 'Bb0', 'C1',
+    'D1', 'E1', 'F1', 'G1', 'A1', 'Bb1', 'C2',
+    'D2', 'E2', 'F2', 'G2', 'A2', 'Bb2', 'C3',
+    'D3', 'E3', 'F3', 'G3', 'A3', 'Bb3', 'C4',
+    'D4', 'E4', 'F4', 'G4', 'A4', 'Bb4', 'C5',
+    'D5', 'E5', 'F5', 'G5', 'A5', 'Bb5', 'C6',
+    'D6', 'E6', 'F6', 'G6', 'A6', 'Bb6', 'C7',
+    'D7', 'E7', 'F7', 'G7', 'A7', 'Bb7', 'C8',
+    'D8', 'E8', 'F8', 'G8', 'A8', 'Bb8', 'C9'
+]
+
+c_sharp_major_notes = [
+    'C#0/Db0', 'D#0/Eb0', 'E#0/F0', 'F#0/Gb0', 'G#0/Ab0', 'A#0/Bb0', 'B#0/C1',
+    'C#1/Db1', 'D#1/Eb1', 'E#1/F1', 'F#1/Gb1', 'G#1/Ab1', 'A#1/Bb1', 'B#1/C2',
+    'C#2/Db2', 'D#2/Eb2', 'E#2/F2', 'F#2/Gb2', 'G#2/Ab2', 'A#2/Bb2', 'B#2/C3',
+    'C#3/Db3', 'D#3/Eb3', 'E#3/F3', 'F#3/Gb3', 'G#3/Ab3', 'A#3/Bb3', 'B#3/C4',
+    'C#4/Db4', 'D#4/Eb4', 'E#4/F4', 'F#4/Gb4', 'G#4/Ab4', 'A#4/Bb4', 'B#4/C5',
+    'C#5/Db5', 'D#5/Eb5', 'E#5/F5', 'F#5/Gb5', 'G#5/Ab5', 'A#5/Bb5', 'B#5/C6',
+    'C#6/Db6', 'D#6/Eb6', 'E#6/F6', 'F#6/Gb6', 'G#6/Ab6', 'A#6/Bb6', 'B#6/C7',
+    'C#7/Db7', 'D#7/Eb7', 'E#7/F7', 'F#7/Gb7', 'G#7/Ab7', 'A#7/Bb7', 'B#7/C8',
+    'C#8/Db8', 'D#8/Eb8', 'E#8/F8', 'F#8/Gb8', 'G#8/Ab8', 'A#8/Bb8', 'B#8/C9'
+]
+
 note_frequencies = {
     'C0': 16.35, 'C#0/Db0': 17.32, 'D0': 18.35, 'D#0/Eb0': 19.45, 'E0': 20.60, 'F0': 21.83, 'F#0/Gb0': 23.12,
     'G0': 24.50, 'G#0/Ab0': 25.96, 'A0': 27.50, 'A#0/Bb0': 29.14, 'B0': 30.87, 
@@ -84,10 +108,88 @@ def play_frequency(frequency, duration=100):
     play_obj.wait_done()
     
 
-file_path = 'ControlFlowFlattening.ipa'
+file_path = 'calc.exe'#'ControlFlowFlattening.ipa'
 read_file_bytes(file_path)
 
-for note in file_notes:
-    if note in c_major_notes:
-        play_frequency(note_frequencies[note], beat_duration_ms)
-        #time.sleep(beat_duration_ms / 1000)  # Convert milliseconds back to seconds for sleep
+# TODO: make the key configurable per sample
+current_key = b_minor_notes
+
+previous_note = file_notes[0]
+num_repeated_notes = 1
+for i in range (1, len(file_notes) - 1):
+    current_note = file_notes[i]
+    
+    
+    if current_note not in current_key: 
+        print("sleeping if")
+        time.sleep((beat_duration_ms / 1000) / 2)  # Convert milliseconds back to seconds for sleep
+    elif current_note == previous_note:
+        print("incrementing num_repeated_notes")
+        num_repeated_notes += 1
+    else:
+        # Play the note
+        print("playing the note!")
+        print("num_repeated_notes:")
+        print(num_repeated_notes)
+        play_frequency(note_frequencies[current_note], num_repeated_notes * beat_duration_ms)
+        num_repeated_notes = 1
+        
+    print("previous_note:")
+    print(previous_note)
+    print("current_note:")
+    print(current_note)
+    previous_note = current_note
+    
+    
+# TODO: add output to midi + output sheet music (lilypond)
+
+
+# TODO: change key + index per file type
+# Each file type should get respective key and also map to frequency range
+
+
+"""
+Base on first letter of the file type?
+
+Supported file types:
+PE
+IPA
+APK
+ELF
+TXT
+Macho
+DLL
+.SO
+DEX
+ASM
+
+
+Keys:
+
+C Major
+C♯ Major / D♭ Major
+D Major
+D♯ Major / E♭ Major
+E Major
+F Major
+F♯ Major / G♭ Major
+G Major
+G♯ Major / A♭ Major
+A Major
+A♯ Major / B♭ Major
+B Major
+Minor Keys:
+
+A Minor
+A♯ Minor / B♭ Minor
+B Minor
+C Minor
+C♯ Minor / D♭ Minor
+D Minor
+D♯ Minor / E♭ Minor
+E Minor
+F Minor
+F♯ Minor / G♭ Minor
+G Minor
+G♯ Minor / A♭ Minor
+"""

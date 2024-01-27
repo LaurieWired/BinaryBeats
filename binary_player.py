954b82c5b6a0bc7005a1e28e4ffafcd2ba380ed1
LaurieWired <123765654+LaurieWired@users.noreply.github.com>
2024-01-26 17:22:02 -0800

    Added remaining keys

--- binary_player.py
+++ binary_player.py
@@ -4,6 +4,28 @@ import simpleaudio as sa
 import io
 import time
 
+g_major_notes = [
+    'G0', 'A0', 'B0', 'C1', 'D1', 'E1', 'F#1/Gb1',
+    'G1', 'A1', 'B1', 'C2', 'D2', 'E2', 'F#2/Gb2',
+    'G2', 'A2', 'B2', 'C3', 'D3', 'E3', 'F#3/Gb3',
+    'G3', 'A3', 'B3', 'C4', 'D4', 'E4', 'F#4/Gb4',
+    'G4', 'A4', 'B4', 'C5', 'D5', 'E5', 'F#5/Gb5',
+    'G5', 'A5', 'B5', 'C6', 'D6', 'E6', 'F#6/Gb6',
+    'G6', 'A6', 'B6', 'C7', 'D7', 'E7', 'F#7/Gb7',
+    'G7', 'A7', 'B7', 'C8', 'D8', 'E8', 'F#8/Gb8'
+]
+
+g_minor_notes = [
+    'G0', 'A0', 'Bb0', 'C1', 'D1', 'Eb1', 'F1',
+    'G1', 'A1', 'Bb1', 'C2', 'D2', 'Eb2', 'F2',
+    'G2', 'A2', 'Bb2', 'C3', 'D3', 'Eb3', 'F3',
+    'G3', 'A3', 'Bb3', 'C4', 'D4', 'Eb4', 'F4',
+    'G4', 'A4', 'Bb4', 'C5', 'D5', 'Eb5', 'F5',
+    'G5', 'A5', 'Bb5', 'C6', 'D6', 'Eb6', 'F6',
+    'G6', 'A6', 'Bb6', 'C7', 'D7', 'Eb7', 'F7',
+    'G7', 'A7', 'Bb7', 'C8', 'D8', 'Eb8', 'F8'
+]
+
 c_major_notes = [
     'C0', 'D0', 'E0', 'F0', 'G0', 'A0', 'B0',
     'C1', 'D1', 'E1', 'F1', 'G1', 'A1', 'B1',
@@ -16,6 +38,53 @@ c_major_notes = [
     'C8', 'D8', 'E8', 'F8', 'G8', 'A8', 'B8'
 ]
 
+c_minor_notes = [
+    'C0', 'D0', 'Eb0', 'F0', 'G0', 'Ab0', 'Bb0',
+    'C1', 'D1', 'Eb1', 'F1', 'G1', 'Ab1', 'Bb1',
+    'C2', 'D2', 'Eb2', 'F2', 'G2', 'Ab2', 'Bb2',
+    'C3', 'D3', 'Eb3', 'F3', 'G3', 'Ab3', 'Bb3',
+    'C4', 'D4', 'Eb4', 'F4', 'G4', 'Ab4', 'Bb4',
+    'C5', 'D5', 'Eb5', 'F5', 'G5', 'Ab5', 'Bb5',
+    'C6', 'D6', 'Eb6', 'F6', 'G6', 'Ab6', 'Bb6',
+    'C7', 'D7', 'Eb7', 'F7', 'G7', 'Ab7', 'Bb7',
+    'C8', 'D8', 'Eb8', 'F8', 'G8', 'Ab8', 'Bb8'
+]
+
+c_sharp_major_notes = [
+    'C#0/Db0', 'D#0/Eb0', 'E#0/F0', 'F#0/Gb0', 'G#0/Ab0', 'A#0/Bb0', 'B#0/C1',
+    'C#1/Db1', 'D#1/Eb1', 'E#1/F1', 'F#1/Gb1', 'G#1/Ab1', 'A#1/Bb1', 'B#1/C2',
+    'C#2/Db2', 'D#2/Eb2', 'E#2/F2', 'F#2/Gb2', 'G#2/Ab2', 'A#2/Bb2', 'B#2/C3',
+    'C#3/Db3', 'D#3/Eb3', 'E#3/F3', 'F#3/Gb3', 'G#3/Ab3', 'A#3/Bb3', 'B#3/C4',
+    'C#4/Db4', 'D#4/Eb4', 'E#4/F4', 'F#4/Gb4', 'G#4/Ab4', 'A#4/Bb4', 'B#4/C5',
+    'C#5/Db5', 'D#5/Eb5', 'E#5/F5', 'F#5/Gb5', 'G#5/Ab5', 'A#5/Bb5', 'B#5/C6',
+    'C#6/Db6', 'D#6/Eb6', 'E#6/F6', 'F#6/Gb6', 'G#6/Ab6', 'A#6/Bb6', 'B#6/C7',
+    'C#7/Db7', 'D#7/Eb7', 'E#7/F7', 'F#7/Gb7', 'G#7/Ab7', 'A#7/Bb7', 'B#7/C8',
+    'C#8/Db8', 'D#8/Eb8', 'E#8/F8', 'F#8/Gb8', 'G#8/Ab8', 'A#8/Bb8', 'B#8/C9'
+]
+
+c_sharp_minor_notes = [
+    'C#0/Db0', 'D#0/Eb0', 'E0', 'F#0/Gb0', 'G#0/Ab0', 'A0', 'B0',
+    'C#1/Db1', 'D#1/Eb1', 'E1', 'F#1/Gb1', 'G#1/Ab1', 'A1', 'B1',
+    'C#2/Db2', 'D#2/Eb2', 'E2', 'F#2/Gb2', 'G#2/Ab2', 'A2', 'B2',
+    'C#3/Db3', 'D#3/Eb3', 'E3', 'F#3/Gb3', 'G#3/Ab3', 'A3', 'B3',
+    'C#4/Db4', 'D#4/Eb4', 'E4', 'F#4/Gb4', 'G#4/Ab4', 'A4', 'B4',
+    'C#5/Db5', 'D#5/Eb5', 'E5', 'F#5/Gb5', 'G#5/Ab5', 'A5', 'B5',
+    'C#6/Db6', 'D#6/Eb6', 'E6', 'F#6/Gb6', 'G#6/Ab6', 'A6', 'B6',
+    'C#7/Db7', 'D#7/Eb7', 'E7', 'F#7/Gb7', 'G#7/Ab7', 'A7', 'B7',
+    'C#8/Db8', 'D#8/Eb8', 'E8', 'F#8/Gb8', 'G#8/Ab8', 'A8', 'B8'
+]
+
+b_major_notes = [
+    'B0', 'C#1/Db1', 'D#1/Eb1', 'E1', 'F#1/Gb1', 'G#1/Ab1', 'A#1/Bb1',
+    'B1', 'C#2/Db2', 'D#2/Eb2', 'E2', 'F#2/Gb2', 'G#2/Ab2', 'A#2/Bb2',
+    'B2', 'C#3/Db3', 'D#3/Eb3', 'E3', 'F#3/Gb3', 'G#3/Ab3', 'A#3/Bb3',
+    'B3', 'C#4/Db4', 'D#4/Eb4', 'E4', 'F#4/Gb4', 'G#4/Ab4', 'A#4/Bb4',
+    'B4', 'C#5/Db5', 'D#5/Eb5', 'E5', 'F#5/Gb5', 'G#5/Ab5', 'A#5/Bb5',
+    'B5', 'C#6/Db6', 'D#6/Eb6', 'E6', 'F#6/Gb6', 'G#6/Ab6', 'A#6/Bb6',
+    'B6', 'C#7/Db7', 'D#7/Eb7', 'E7', 'F#7/Gb7', 'G#7/Ab7', 'A#7/Bb7',
+    'B7', 'C#8/Db8', 'D#8/Eb8', 'E8', 'F#8/Gb8', 'G#8/Ab8', 'A#8/Bb8'
+]
+
 b_minor_notes = [
     'B0', 'C#1/Db1', 'D1', 'E1', 'F#1/Gb1', 'G1', 'A1',
     'B1', 'C#2/Db2', 'D2', 'E2', 'F#2/Gb2', 'G2', 'A2',
@@ -25,6 +94,39 @@ b_minor_notes = [
     'B5', 'C#6/Db6', 'D6', 'E6', 'F#6/Gb6', 'G6', 'A6',
     'B6', 'C#7/Db7', 'D7', 'E7', 'F#7/Gb7', 'G7', 'A7',
     'B7', 'C#8/Db8', 'D8', 'E8', 'F#8/Gb8', 'G8', 'A8'
+]
+
+bb_major_notes = [
+    'Bb0/A#0', 'C1', 'D1', 'Eb1/D#1', 'F1', 'G1', 'A1',
+    'Bb1/A#1', 'C2', 'D2', 'Eb2/D#2', 'F2', 'G2', 'A2',
+    'Bb2/A#2', 'C3', 'D3', 'Eb3/D#3', 'F3', 'G3', 'A3',
+    'Bb3/A#3', 'C4', 'D4', 'Eb4/D#4', 'F4', 'G4', 'A4',
+    'Bb4/A#4', 'C5', 'D5', 'Eb5/D#5', 'F5', 'G5', 'A5',
+    'Bb5/A#5', 'C6', 'D6', 'Eb6/D#6', 'F6', 'G6', 'A6',
+    'Bb6/A#6', 'C7', 'D7', 'Eb7/D#7', 'F7', 'G7', 'A7',
+    'Bb7/A#7', 'C8', 'D8', 'Eb8/D#8', 'F8', 'G8', 'A8'
+]
+
+bb_minor_notes = [
+    'Bb0/A#0', 'C1', 'Db1/C#1', 'Eb1/D#1', 'F1', 'Gb1/F#1', 'Ab1/G#1',
+    'Bb1/A#1', 'C2', 'Db2/C#2', 'Eb2/D#2', 'F2', 'Gb2/F#2', 'Ab2/G#2',
+    'Bb2/A#2', 'C3', 'Db3/C#3', 'Eb3/D#3', 'F3', 'Gb3/F#3', 'Ab3/G#3',
+    'Bb3/A#3', 'C4', 'Db4/C#4', 'Eb4/D#4', 'F4', 'Gb4/F#4', 'Ab4/G#4',
+    'Bb4/A#4', 'C5', 'Db5/C#5', 'Eb5/D#5', 'F5', 'Gb5/F#5', 'Ab5/G#5',
+    'Bb5/A#5', 'C6', 'Db6/C#6', 'Eb6/D#6', 'F6', 'Gb6/F#6', 'Ab6/G#6',
+    'Bb6/A#6', 'C7', 'Db7/C#7', 'Eb7/D#7', 'F7', 'Gb7/F#7', 'Ab7/G#7',
+    'Bb7/A#7', 'C8', 'Db8/C#8', 'Eb8/D#8', 'F8', 'Gb8/F#8', 'Ab8/G#8'
+]
+
+d_major_notes = [
+    'D0', 'E0', 'F#0/Gb0', 'G0', 'A0', 'B0', 'C#1/Db1',
+    'D1', 'E1', 'F#1/Gb1', 'G1', 'A1', 'B1', 'C#2/Db2',
+    'D2', 'E2', 'F#2/Gb2', 'G2', 'A2', 'B2', 'C#3/Db3',
+    'D3', 'E3', 'F#3/Gb3', 'G3', 'A3', 'B3', 'C#4/Db4',
+    'D4', 'E4', 'F#4/Gb4', 'G4', 'A4', 'B4', 'C#5/Db5',
+    'D5', 'E5', 'F#5/Gb5', 'G5', 'A5', 'B5', 'C#6/Db6',
+    'D6', 'E6', 'F#6/Gb6', 'G6', 'A6', 'B6', 'C#7/Db7',
+    'D7', 'E7', 'F#7/Gb7', 'G7', 'A7', 'B7', 'C#8/Db8'
 ]
 
 d_minor_notes = [
@@ -39,17 +141,183 @@ d_minor_notes = [
     'D8', 'E8', 'F8', 'G8', 'A8', 'Bb8', 'C9'
 ]
 
-c_sharp_major_notes = [
-    'C#0/Db0', 'D#0/Eb0', 'E#0/F0', 'F#0/Gb0', 'G#0/Ab0', 'A#0/Bb0', 'B#0/C1',
-    'C#1/Db1', 'D#1/Eb1', 'E#1/F1', 'F#1/Gb1', 'G#1/Ab1', 'A#1/Bb1', 'B#1/C2',
-    'C#2/Db2', 'D#2/Eb2', 'E#2/F2', 'F#2/Gb2', 'G#2/Ab2', 'A#2/Bb2', 'B#2/C3',
-    'C#3/Db3', 'D#3/Eb3', 'E#3/F3', 'F#3/Gb3', 'G#3/Ab3', 'A#3/Bb3', 'B#3/C4',
-    'C#4/Db4', 'D#4/Eb4', 'E#4/F4', 'F#4/Gb4', 'G#4/Ab4', 'A#4/Bb4', 'B#4/C5',
-    'C#5/Db5', 'D#5/Eb5', 'E#5/F5', 'F#5/Gb5', 'G#5/Ab5', 'A#5/Bb5', 'B#5/C6',
-    'C#6/Db6', 'D#6/Eb6', 'E#6/F6', 'F#6/Gb6', 'G#6/Ab6', 'A#6/Bb6', 'B#6/C7',
-    'C#7/Db7', 'D#7/Eb7', 'E#7/F7', 'F#7/Gb7', 'G#7/Ab7', 'A#7/Bb7', 'B#7/C8',
-    'C#8/Db8', 'D#8/Eb8', 'E#8/F8', 'F#8/Gb8', 'G#8/Ab8', 'A#8/Bb8', 'B#8/C9'
+db_minor_notes = [
+    'Db0/C#0', 'Eb0/D#0', 'E0', 'F0', 'Gb0/F#0', 'Ab0/G#0', 'A0',
+    'Db1/C#1', 'Eb1/D#1', 'E1', 'F1', 'Gb1/F#1', 'Ab1/G#1', 'A1',
+    'Db2/C#2', 'Eb2/D#2', 'E2', 'F2', 'Gb2/F#2', 'Ab2/G#2', 'A2',
+    'Db3/C#3', 'Eb3/D#3', 'E3', 'F3', 'Gb3/F#3', 'Ab3/G#3', 'A3',
+    'Db4/C#4', 'Eb4/D#4', 'E4', 'F4', 'Gb4/F#4', 'Ab4/G#4', 'A4',
+    'Db5/C#5', 'Eb5/D#5', 'E5', 'F5', 'Gb5/F#5', 'Ab5/G#5', 'A5',
+    'Db6/C#6', 'Eb6/D#6', 'E6', 'F6', 'Gb6/F#6', 'Ab6/G#6', 'A6',
+    'Db7/C#7', 'Eb7/D#7', 'E7', 'F7', 'Gb7/F#7', 'Ab7/G#7', 'A7',
+    'Db8/C#8', 'Eb8/D#8', 'E8', 'F8', 'Gb8/F#8', 'Ab8/G#8', 'A8'
 ]
+
+eb_major_notes = [
+    'Eb0/D#0', 'F0', 'G0', 'Ab0/G#0', 'Bb0/A#0', 'C1', 'D1',
+    'Eb1/D#1', 'F1', 'G1', 'Ab1/G#1', 'Bb1/A#1', 'C2', 'D2',
+    'Eb2/D#2', 'F2', 'G2', 'Ab2/G#2', 'Bb2/A#2', 'C3', 'D3',
+    'Eb3/D#3', 'F3', 'G3', 'Ab3/G#3', 'Bb3/A#3', 'C4', 'D4',
+    'Eb4/D#4', 'F4', 'G4', 'Ab4/G#4', 'Bb4/A#4', 'C5', 'D5',
+    'Eb5/D#5', 'F5', 'G5', 'Ab5/G#5', 'Bb5/A#5', 'C6', 'D6',
+    'Eb6/D#6', 'F6', 'G6', 'Ab6/G#6', 'Bb6/A#6', 'C7', 'D7',
+    'Eb7/D#7', 'F7', 'G7', 'Ab7/G#7', 'Bb7/A#7', 'C8', 'D8',
+    'Eb8/D#8', 'F8', 'G8', 'Ab8/G#8', 'Bb8/A#8'
+]
+
+eb_minor_notes = [
+    'Eb0/D#0', 'F0', 'Gb0/F#0', 'Ab0/G#0', 'Bb0/A#0', 'Cb1/B0', 'Db1/C#1',
+    'Eb1/D#1', 'F1', 'Gb1/F#1', 'Ab1/G#1', 'Bb1/A#1', 'Cb2/B1', 'Db2/C#2',
+    'Eb2/D#2', 'F2', 'Gb2/F#2', 'Ab2/G#2', 'Bb2/A#2', 'Cb3/B2', 'Db3/C#3',
+    'Eb3/D#3', 'F3', 'Gb3/F#3', 'Ab3/G#3', 'Bb3/A#3', 'Cb4/B3', 'Db4/C#4',
+    'Eb4/D#4', 'F4', 'Gb4/F#4', 'Ab4/G#4', 'Bb4/A#4', 'Cb5/B4', 'Db5/C#5',
+    'Eb5/D#5', 'F5', 'Gb5/F#5', 'Ab5/G#5', 'Bb5/A#5', 'Cb6/B5', 'Db6/C#6',
+    'Eb6/D#6', 'F6', 'Gb6/F#6', 'Ab6/G#6', 'Bb6/A#6', 'Cb7/B6', 'Db7/C#7',
+    'Eb7/D#7', 'F7', 'Gb7/F#7', 'Ab7/G#7', 'Bb7/A#7', 'Cb8/B7', 'Db8/C#8',
+    'Eb8/D#8', 'F8', 'Gb8/F#8', 'Ab8/G#8', 'Bb8/A#8'
+]
+
+e_major_notes = [
+    'E0', 'F#0/Gb0', 'G#0/Ab0', 'A0', 'B0', 'C#1/Db1', 'D#1/Eb1',
+    'E1', 'F#1/Gb1', 'G#1/Ab1', 'A1', 'B1', 'C#2/Db2', 'D#2/Eb2',
+    'E2', 'F#2/Gb2', 'G#2/Ab2', 'A2', 'B2', 'C#3/Db3', 'D#3/Eb3',
+    'E3', 'F#3/Gb3', 'G#3/Ab3', 'A3', 'B3', 'C#4/Db4', 'D#4/Eb4',
+    'E4', 'F#4/Gb4', 'G#4/Ab4', 'A4', 'B4', 'C#5/Db5', 'D#5/Eb5',
+    'E5', 'F#5/Gb5', 'G#5/Ab5', 'A5', 'B5', 'C#6/Db6', 'D#6/Eb6',
+    'E6', 'F#6/Gb6', 'G#6/Ab6', 'A6', 'B6', 'C#7/Db7', 'D#7/Eb7',
+    'E7', 'F#7/Gb7', 'G#7/Ab7', 'A7', 'B7', 'C#8/Db8', 'D#8/Eb8'
+]
+
+e_minor_notes = [
+    'E0', 'F#0/Gb0', 'G0', 'A0', 'B0', 'C1', 'D1',
+    'E1', 'F#1/Gb1', 'G1', 'A1', 'B1', 'C2', 'D2',
+    'E2', 'F#2/Gb2', 'G2', 'A2', 'B2', 'C3', 'D3',
+    'E3', 'F#3/Gb3', 'G3', 'A3', 'B3', 'C4', 'D4',
+    'E4', 'F#4/Gb4', 'G4', 'A4', 'B4', 'C5', 'D5',
+    'E5', 'F#5/Gb5', 'G5', 'A5', 'B5', 'C6', 'D6',
+    'E6', 'F#6/Gb6', 'G6', 'A6', 'B6', 'C7', 'D7',
+    'E7', 'F#7/Gb7', 'G7', 'A7', 'B7', 'C8', 'D8'
+]
+
+f_major_notes = [
+    'F0', 'G0', 'A0', 'Bb0', 'C1', 'D1', 'E1',
+    'F1', 'G1', 'A1', 'Bb1', 'C2', 'D2', 'E2',
+    'F2', 'G2', 'A2', 'Bb2', 'C3', 'D3', 'E3',
+    'F3', 'G3', 'A3', 'Bb3', 'C4', 'D4', 'E4',
+    'F4', 'G4', 'A4', 'Bb4', 'C5', 'D5', 'E5',
+    'F5', 'G5', 'A5', 'Bb5', 'C6', 'D6', 'E6',
+    'F6', 'G6', 'A6', 'Bb6', 'C7', 'D7', 'E7',
+    'F7', 'G7', 'A7', 'Bb7', 'C8', 'D8', 'E8'
+]
+
+f_minor_notes = [
+    'F0', 'G0', 'Ab0', 'Bb0', 'C1', 'Db1', 'Eb1',
+    'F1', 'G1', 'Ab1', 'Bb1', 'C2', 'Db2', 'Eb2',
+    'F2', 'G2', 'Ab2', 'Bb2', 'C3', 'Db3', 'Eb3',
+    'F3', 'G3', 'Ab3', 'Bb3', 'C4', 'Db4', 'Eb4',
+    'F4', 'G4', 'Ab4', 'Bb4', 'C5', 'Db5', 'Eb5',
+    'F5', 'G5', 'Ab5', 'Bb5', 'C6', 'Db6', 'Eb6',
+    'F6', 'G6', 'Ab6', 'Bb6', 'C7', 'Db7', 'Eb7',
+    'F7', 'G7', 'Ab7', 'Bb7', 'C8', 'Db8', 'Eb8'
+]
+
+f_sharp_major_notes = [
+    'F#0/Gb0', 'G#0/Ab0', 'A#0/Bb0', 'B0', 'C#1/Db1', 'D#1/Eb1', 'E#1/F1',
+    'F#1/Gb1', 'G#1/Ab1', 'A#1/Bb1', 'B1', 'C#2/Db2', 'D#2/Eb2', 'E#2/F2',
+    'F#2/Gb2', 'G#2/Ab2', 'A#2/Bb2', 'B2', 'C#3/Db3', 'D#3/Eb3', 'E#3/F3',
+    'F#3/Gb3', 'G#3/Ab3', 'A#3/Bb3', 'B3', 'C#4/Db4', 'D#4/Eb4', 'E#4/F4',
+    'F#4/Gb4', 'G#4/Ab4', 'A#4/Bb4', 'B4', 'C#5/Db5', 'D#5/Eb5', 'E#5/F5',
+    'F#5/Gb5', 'G#5/Ab5', 'A#5/Bb5', 'B5', 'C#6/Db6', 'D#6/Eb6', 'E#6/F6',
+    'F#6/Gb6', 'G#6/Ab6', 'A#6/Bb6', 'B6', 'C#7/Db7', 'D#7/Eb7', 'E#7/F7',
+    'F#7/Gb7', 'G#7/Ab7', 'A#7/Bb7', 'B7', 'C#8/Db8', 'D#8/Eb8', 'E#8/F8'
+]
+
+f_sharp_minor_notes = [
+    'F#0/Gb0', 'G#0/Ab0', 'A0', 'B0', 'C#1/Db1', 'D1', 'E1',
+    'F#1/Gb1', 'G#1/Ab1', 'A1', 'B1', 'C#2/Db2', 'D2', 'E2',
+    'F#2/Gb2', 'G#2/Ab2', 'A2', 'B2', 'C#3/Db3', 'D3', 'E3',
+    'F#3/Gb3', 'G#3/Ab3', 'A3', 'B3', 'C#4/Db4', 'D4', 'E4',
+    'F#4/Gb4', 'G#4/Ab4', 'A4', 'B4', 'C#5/Db5', 'D5', 'E5',
+    'F#5/Gb5', 'G#5/Ab5', 'A5', 'B5', 'C#6/Db6', 'D6', 'E6',
+    'F#6/Gb6', 'G#6/Ab6', 'A6', 'B6', 'C#7/Db7', 'D7', 'E7',
+    'F#7/Gb7', 'G#7/Ab7', 'A7', 'B7', 'C#8/Db8', 'D8', 'E8'
+]
+
+a_major_notes = [
+    'A0', 'B0', 'C#1/Db1', 'D1', 'E1', 'F#1/Gb1', 'G#1/Ab1',
+    'A1', 'B1', 'C#2/Db2', 'D2', 'E2', 'F#2/Gb2', 'G#2/Ab2',
+    'A2', 'B2', 'C#3/Db3', 'D3', 'E3', 'F#3/Gb3', 'G#3/Ab3',
+    'A3', 'B3', 'C#4/Db4', 'D4', 'E4', 'F#4/Gb4', 'G#4/Ab4',
+    'A4', 'B4', 'C#5/Db5', 'D5', 'E5', 'F#5/Gb5', 'G#5/Ab5',
+    'A5', 'B5', 'C#6/Db6', 'D6', 'E6', 'F#6/Gb6', 'G#6/Ab6',
+    'A6', 'B6', 'C#7/Db7', 'D7', 'E7', 'F#7/Gb7', 'G#7/Ab7',
+    'A7', 'B7', 'C#8/Db8', 'D8', 'E8', 'F#8/Gb8', 'G#8/Ab8'
+]
+
+a_minor_notes = [
+    'A0', 'B0', 'C1', 'D1', 'E1', 'F1', 'G1',
+    'A1', 'B1', 'C2', 'D2', 'E2', 'F2', 'G2',
+    'A2', 'B2', 'C3', 'D3', 'E3', 'F3', 'G3',
+    'A3', 'B3', 'C4', 'D4', 'E4', 'F4', 'G4',
+    'A4', 'B4', 'C5', 'D5', 'E5', 'F5', 'G5',
+    'A5', 'B5', 'C6', 'D6', 'E6', 'F6', 'G6',
+    'A6', 'B6', 'C7', 'D7', 'E7', 'F7', 'G7',
+    'A7', 'B7', 'C8', 'D8', 'E8', 'F8', 'G8'
+]
+
+ab_major_notes = [
+    'Ab0/G#0', 'Bb0/A#0', 'C1', 'Db1/C#1', 'Eb1/D#1', 'F1', 'G1',
+    'Ab1/G#1', 'Bb1/A#1', 'C2', 'Db2/C#2', 'Eb2/D#2', 'F2', 'G2',
+    'Ab2/G#2', 'Bb2/A#2', 'C3', 'Db3/C#3', 'Eb3/D#3', 'F3', 'G3',
+    'Ab3/G#3', 'Bb3/A#3', 'C4', 'Db4/C#4', 'Eb4/D#4', 'F4', 'G4',
+    'Ab4/G#4', 'Bb4/A#4', 'C5', 'Db5/C#5', 'Eb5/D#5', 'F5', 'G5',
+    'Ab5/G#5', 'Bb5/A#5', 'C6', 'Db6/C#6', 'Eb6/D#6', 'F6', 'G6',
+    'Ab6/G#6', 'Bb6/A#6', 'C7', 'Db7/C#7', 'Eb7/D#7', 'F7', 'G7',
+    'Ab7/G#7', 'Bb7/A#7', 'C8', 'Db8/C#8', 'Eb8/D#8', 'F8', 'G8'
+]
+
+ab_minor_notes = [
+    'Ab0/G#0', 'Bb0/A#0', 'B0', 'Db1/C#1', 'Eb1/D#1', 'E1', 'Gb1/F#1',
+    'Ab1/G#1', 'Bb1/A#1', 'B1', 'Db2/C#2', 'Eb2/D#2', 'E2', 'Gb2/F#2',
+    'Ab2/G#2', 'Bb2/A#2', 'B2', 'Db3/C#3', 'Eb3/D#3', 'E3', 'Gb3/F#3',
+    'Ab3/G#3', 'Bb3/A#3', 'B3', 'Db4/C#4', 'Eb4/D#4', 'E4', 'Gb4/F#4',
+    'Ab4/G#4', 'Bb4/A#4', 'B4', 'Db5/C#5', 'Eb5/D#5', 'E5', 'Gb5/F#5',
+    'Ab5/G#5', 'Bb5/A#5', 'B5', 'Db6/C#6', 'Eb6/D#6', 'E6', 'Gb6/F#6',
+    'Ab6/G#6', 'Bb6/A#6', 'B6', 'Db7/C#7', 'Eb7/D#7', 'E7', 'Gb7/F#7',
+    'Ab7/G#7', 'Bb7/A#7', 'B7', 'Db8/C#8', 'Eb8/D#8', 'E8', 'Gb8/F#8'
+]
+
+"""
+Summary
+
+    All 12 major scales:
+        C Major
+        G Major
+        D Major
+        A Major
+        E Major
+        B Major
+        F# (Gb) Major
+        Db (C#) Major
+        Ab (G#) Major
+        Eb (D#) Major
+        Bb (A#) Major
+        F Major
+
+    All 12 natural minor scales:
+        A Minor
+        E Minor
+        B Minor
+        F# (Gb) Minor
+        C# (Db) Minor
+        G# (Ab) Minor
+        Eb (D#) Minor
+        Bb (A#) Minor
+        F Minor
+        C Minor
+        G Minor
+        D Minor
+"""
 
 note_frequencies = {
     'C0': 16.35, 'C#0/Db0': 17.32, 'D0': 18.35, 'D#0/Eb0': 19.45, 'E0': 20.60, 'F0': 21.83, 'F#0/Gb0': 23.12,
@@ -144,12 +412,8 @@ for i in range (1, len(file_notes) - 1):
 # TODO: add output to midi + output sheet music (lilypond)
 
 
-# TODO: change key + index per file type
-# Each file type should get respective key and also map to frequency range
-
-
 """
-Base on first letter of the file type?
+TODO: map scales to different file types + frequency range
 
 Supported file types:
 PE
@@ -163,33 +427,4 @@ DLL
 DEX
 ASM
 
-
-Keys:
-
-C Major
-C♯ Major / D♭ Major
-D Major
-D♯ Major / E♭ Major
-E Major
-F Major
-F♯ Major / G♭ Major
-G Major
-G♯ Major / A♭ Major
-A Major
-A♯ Major / B♭ Major
-B Major
-Minor Keys:
-
-A Minor
-A♯ Minor / B♭ Minor
-B Minor
-C Minor
-C♯ Minor / D♭ Minor
-D Minor
-D♯ Minor / E♭ Minor
-E Minor
-F Minor
-F♯ Minor / G♭ Minor
-G Minor
-G♯ Minor / A♭ Minor
 """
